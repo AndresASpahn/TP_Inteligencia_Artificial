0b9cc3cb84733a59e1075d1040d41853df9478ee
Manuel Musso <manumusso05@gmail.com>
2021-11-01 00:03:08 -0300

    Variables + Dominio problema 2

---
+++ entrega2.py
@@ -0,0 +1,42 @@
+from simpleai.search import (CspProblem, backtrack)
+from itertools import combinations
+
+
+#Variables del problema definidas según funcionalidades solicitadas
+problem_variables = ["incrementar_autonomia","terreno_irregulares","cargas_extras","comunicacion_robot"]
+domains ={}
+
+#Dominio correspondiente al incremento de autonomia local
+# (Desc),(nuevaCarga),(Costo movimiento extra)
+
+domains[problem_variables[0]] =[
+    ("bateria_chica"),(500),(10),
+    ("bateria_mediana"),(7500),(20),
+    ("bateria_grande"),(10000),(50)]
+
+#Adaptación Terrenos irregulares
+#(nombre,costo_extra)
+
+domains[problem_variables[1]] =[
+    (("patas_extras"),(15)),
+    (("mot_potente"),(25)),
+    (("orugas"),(50))]
+
+#Suministros 
+#(descripcion),(consumoxmovimiento)
+domains[problem_variables[2]] =[
+    (("suministro_hum_tras"),(10)),
+    (("suministro_hum_sup"),(10)),]
+
+#Comunicación con robot 
+#(nombre),(costo_consumo por movimiento)
+domains[problem_variables[3]] =[
+    (("videollamada"),(10),)]
+
+print (domains[problem_variables[0]]) 
+print (domains[problem_variables[1]])    
+print (domains[problem_variables[2]]) 
+print (domains[problem_variables[3]]) 
+constraints = []
+def rediseñar_robot():
+ return "ok"
