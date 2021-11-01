0bd3db7c3f25b62c53c477608d3bcdde65b5fb99
Manuel Musso <manumusso05@gmail.com>
2021-11-01 01:32:54 -0300

    Dominio y funcion de adaptacion

--- entrega2.py
+++ entrega2.py
@@ -10,39 +10,52 @@ domains ={}
 # (Desc),(nuevaCarga),(Costo movimiento extra)
 
 domains[problem_variables[0]] =[
-    ("bateria_chica"),(500),(10),
-    ("bateria_mediana"),(7500),(20),
-    ("bateria_grande"),(10000),(50)]
+    (("baterias_chicas"),(500),(10)),
+    (("baterias_medianas"),(7500),(20)),
+    (("baterias_grandes"),(10000),(50))]
 
 #Adaptación Terrenos irregulares
 #(nombre,costo_extra)
 
 domains[problem_variables[1]] =[
     (("patas_extras"),(15)),
-    (("mot_potente"),(25)),
+    (("mejores_motores"),(25)),
     (("orugas"),(50))]
 
 #Suministros 
 #(descripcion),(consumoxmovimiento)
 domains[problem_variables[2]] =[
-    (("suministro_hum_tras"),(10)),
-    (("suministro_hum_sup"),(10)),]
+    (("caja_superior"),(10)),
+    (("caja_trasera"),(10)),]
 
 #Comunicación con robot 
 #(nombre),(costo_consumo por movimiento)
 domains[problem_variables[3]] =[
-    (("videollamada"),(10),)]
+    (("videollamada"),(10)),(("radio"),(5))]
 
 #print (domains[problem_variables[0]]) 
 #print (domains[problem_variables[1]])    
 #print (domains[problem_variables[2]]) 
 #print (domains[problem_variables[3]]) 
 
-
 constraints= []
 
 # Aca vamos a definir la función para la adaptación
 def rediseñar_robot():
-    problem = CspProblem(problem_variables, domains, constraints)
-    pass
+    problema = CspProblem(problem_variables, domains, constraints)
+    solucion = backtrack(problema) 
+    solucion = list(solucion.values())
 
+    lista_adaptaciones = []
+    for adaptacion in solucion:
+        lista_adaptaciones.append(adaptacion[0])
+    return lista_adaptaciones
+    
+if __name__ == '__main__':
+    
+    import os
+    import sys
+
+    print(os.path.dirname(sys.executable))  
+    prueba = rediseñar_robot()
+    print(prueba)
