6d7e6bceb7bb5585e802e3db33660a1e9acef2f8
Manuel Musso <manumusso05@gmail.com>
2021-11-02 18:42:06 -0300

    fix2,falta ver el caso de autonomía

--- entrega2.py
+++ entrega2.py
@@ -14,15 +14,15 @@ baterias_base = 1000
 domains = {
     'incrementar_autonomia':
     [
-        ('baterias_chicas',10,4000),
-        ('baterias_medianas',20,6500),
-        ('baterias_grandes',50,9000),
+        ('baterias_chicas',10,5000),
+        ('baterias_medianas',20,7500),
+        ('baterias_grandes',50,10000),
     ]
     ,'terreno_irregulares':
     [
         ('patas_extras',15),
         ('mejores_motores',25),
-        ('orugas',50,0),
+        ('orugas',50),
     ]
     ,'cargas_extras':
     [
@@ -39,61 +39,75 @@ domains = {
 
 constraints=[]
 
+#Las baterias  grandes solo se utilizan si tiene las orugas
 def es_adm_uruga(variables,values):
     incrementar_autonomia,terreno = values
-    has_bateriasgrandes = "baterias_grandes" in incrementar_autonomia[0]
-    has_orugas = "mejores_motores" in terreno[0]
+    has_bateriasgrandes = 'baterias_grandes' in incrementar_autonomia[0]
+    has_orugas = 'orugas' in terreno[0]
 
     if (has_bateriasgrandes):
         return has_orugas
     else:
+        #Si no es bateria grande devolvemos true.
         return True
 
-#Función para controlar la autonomía.
-def es_autonomo(variables, values):
-    var= values[0]
-    x=0
-    #Almaceno la carga total de la bateria.
-    carga_t = var[2]
-    consumo_resultante = 0
-    consumo_x_minuto = 0
-    #Acumulo los costos de cada movimiento extras y además le sumo el base. 
-    for x in values:
-        consumo_x_minuto += x[1]
-
-    consumo_resultante= consumo_x_minuto + consumo_base
-    bateria_resultante = carga_t + baterias_base
-
-    return ((bateria_resultante/consumo_resultante) >= 50)
-
-#Controlar la autonomía.
-
+#Radio no es compatible con motores potentes.
 def es_compatible_motor_radio(variables,values):
     terrenos,comunicacion = values
     #Si hay  mejores motores no puede haber radios.
-    has_mejora = "mejores_motores" in terrenos[0]
-    has_radio = "radios" in comunicacion[0]
-    if has_mejora:
-        return not has_radio
+    has_mejora = 'mejores_motores' in terrenos[0]
+    has_radio = 'radios' in comunicacion[0]
+    if has_radio:
+        return not has_mejora
     else:
         return True
-
 
 # Restricción: si usa sistema de videollamadas necesita el par extra de patas o las orugas
 def es_compatible_videollamada(variables,values):
     terrenos,comunicacion = values
-    comunic = "video_llamadas" in comunicacion[0] 
+    comunic = 'video_llamadas' in comunicacion[0] 
     terre= terrenos[0] in('patas_extras','orugas')
     if comunic:
         return terre
     else:
         True
+#Caja de suministros humanitarios en la parte trasera. 
+#El peso extra incrementa el consumo de cada movimiento en 10 mAh extras, 
+#y por su ubicación no es compatible con el par extra de patas, que se ubican en la misma región del chasis.
+
+def es_compatible_patas_suminostrostras(variables,values):
+    terreneros,cargas = values
+    has_patas = 'patas_extras' in terreneros[0]
+    has_chasis = 'caja_trasera' in cargas[0]
+    if has_chasis:
+        return not has_patas
+    return True
 
 
- #constraints.append((variables_problem,es_autonomo))
+
+constraints.append((('terreno_irregulares','cargas_extras'),es_compatible_patas_suminostrostras)) 
+
+#Controlar la autonomia
+def es_autonomo(variables,values):
+    var= values[0]
+    consumos_extras = 0
+    bateria = var[2]
+    for x in values:
+        consumos_extras += x[1]
+        print("ConsumoActual:",+consumos_extras)
+    print("Consumo actual caso:",+consumos_extras)
+    print("Bateria",+bateria)
+    consumo_t = 100 + consumos_extras 
+    bateria_resultante = 1000 + bateria
+
+    autonomia = ((bateria_resultante/consumo_t)>= 50)
+    print("Autonomía actual",+(bateria_resultante/consumo_t))
+    return(autonomia)
+
 constraints.append((('incrementar_autonomia','terreno_irregulares'),es_adm_uruga))   
 constraints.append((('terreno_irregulares', 'comunicacion_robot'), es_compatible_motor_radio))
 constraints.append((('terreno_irregulares', 'comunicacion_robot'), es_compatible_videollamada))
+constraints.append(((variables_problem),es_autonomo))
 
 #Función rediseñar.
 # Aca vamos a definir la función para la adaptación
@@ -113,7 +127,5 @@ def rediseñar_robot():
     #print(solution['incrementar_autonomia'][0])
 
 if __name__ == '__main__':
-    problem = CspProblem(variables_problem, domains, constraints)
-    solution = backtrack(problem)
     adaptaciones = rediseñar_robot()
     print(adaptaciones)
