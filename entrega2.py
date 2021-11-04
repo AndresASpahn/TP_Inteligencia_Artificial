7fcf70a27ed896b95c1f80f7b7f1699ebee7123e
Manuel Musso <manumusso05@gmail.com>
2021-11-03 23:18:49 -0300

    Entrega2 terminada

--- entrega2.py
+++ entrega2.py
@@ -10,16 +10,19 @@ consumo_base = 100
 baterias_base = 1000
 
 #Se planteo un diccionario ya que con la anterior estructura teniamos un problema de type al igual solution al backtrack(problem)
-#Se planteo una estructura de diccionario con tuplas adentro. Un indice por cada mejora.
+#Se planteo una estructura de diccionario con tuplas adentro.
+
 domains = {
     'incrementar_autonomia':
     [
+        #desc,consumo,tamañobateria
         ('baterias_chicas',10,4000),
         ('baterias_medianas',20,6500),
         ('baterias_grandes',50,9000),
     ]
     ,'terreno_irregulares':
     [
+        #desc,consumo
         ('patas_extras',15),
         ('mejores_motores',25),
         ('orugas',50),
@@ -42,19 +45,25 @@ constraints=[]
 #Las baterias  grandes solo se utilizan si tiene las orugas
 def es_adm_uruga(variables,values):
     incrementar_autonomia,terreno = values
+
+    #Asigno una variable boolean para comprobar existencia de las baterias grandes
     has_bateriasgrandes = 'baterias_grandes' in incrementar_autonomia[0]
+
+    #Asigno una variable boolean para comprobar existencia de las orugas
     has_orugas = 'orugas' in terreno[0]
 
     if (has_bateriasgrandes):
+    
+        #Si la bateria es grande, retornamos True en caso que este la oruga, caso que no False.
         return has_orugas
     else:
-        #Si no es bateria grande devolvemos true.
+        #Si no es bateria grande, no tenemos porque restringir, devolvemos True
         return True
+
 
 #Radio no es compatible con motores potentes.
 def es_compatible_motor_radio(variables,values):
     terrenos,comunicacion = values
-    #Si hay  mejores motores no puede haber radios.
     has_mejora = 'mejores_motores' in terrenos[0]
     has_radio = 'radios' in comunicacion[0]
     if has_radio:
@@ -65,6 +74,7 @@ def es_compatible_motor_radio(variables,values):
 # Restricción: si usa sistema de videollamadas necesita el par extra de patas o las orugas
 def es_compatible_videollamada(variables,values):
     terrenos,comunicacion = values
+
     comunic = 'video_llamadas' in comunicacion[0] 
     terre= terrenos[0] in('patas_extras','orugas')
     if comunic:
@@ -72,9 +82,9 @@ def es_compatible_videollamada(variables,values):
     else:
         return True
         
-#Caja de suministros humanitarios en la parte trasera. 
-#El peso extra incrementa el consumo de cada movimiento en 10 mAh extras, 
-#y por su ubicación no es compatible con el par extra de patas, que se ubican en la misma región del chasis.
+
+#La caja de suminostros Humanitarios en la parte trasera, 
+# por su ubicación no es compatible con el par extra de patas, que se ubican en la misma región del chasis.
 
 def es_compatible_patas_suminostrostras(variables,values):
     terreneros,cargas = values
@@ -83,10 +93,6 @@ def es_compatible_patas_suminostrostras(variables,values):
     if has_chasis:
         return  not (has_patas)
     return True
-
-
-
-
 
 #Controlar la autonomia
 def es_autonomo(variables,values):
@@ -97,14 +103,10 @@ def es_autonomo(variables,values):
     for x in values:
         consumos_extras += x[1]
   
-        print("ConsumoActual:",+consumos_extras)
-    print("Consumo actual caso:",+consumos_extras)
-    print("Bateria",+bateria)
-    consumo_t = 100 + consumos_extras 
-    bateria_resultante = 1000 + bateria
+    consumo_t = consumo_base + consumos_extras 
+    bateria_resultante = baterias_base + bateria
 
     autonomia = ((bateria_resultante/consumo_t) >= 50)
-    print("Autonomía actual",+(bateria_resultante/consumo_t))
     return(autonomia)
 
 
@@ -114,14 +116,13 @@ constraints.append((('terreno_irregulares', 'comunicacion_robot'),es_compatible_
 constraints.append((('terreno_irregulares', 'comunicacion_robot'),es_compatible_videollamada))
 constraints.append(((variables_problem),es_autonomo))
 
-#Función rediseñar.
-# Aca vamos a definir la función para la adaptación
+#Función adaptacion
 def rediseñar_robot():
     
     problem = CspProblem(variables_problem,domains,constraints)
     solution = backtrack(problem)
     lista_soluciones=[]
-    #print("Solution:")
+
     lista_soluciones.append(solution['incrementar_autonomia'][0])
     lista_soluciones.append(solution['terreno_irregulares'][0])
     lista_soluciones.append(solution['cargas_extras'][0])
@@ -129,8 +130,6 @@ def rediseñar_robot():
 
     return lista_soluciones
     
-    #print(solution['incrementar_autonomia'][0])
-
 if __name__ == '__main__':
     adaptaciones = rediseñar_robot()
     print(adaptaciones)
