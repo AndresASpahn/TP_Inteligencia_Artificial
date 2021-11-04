c592737636221b731772c6c57d1a472be2c7775f
Andres Spahn <andresspahn@gmail.com>
2021-11-04 16:11:36 -0300

    Todos los test corriendo

--- entrega1.py
+++ entrega1.py
@@ -25,16 +25,17 @@ def planear_escaneo(tuneles, robots):
             lista_acciones = []
 
             for robot in lrobots:
-                if robot[1] == "escaneador"and robot[2] >= 100:
-                    for mov in movimientos_posibles:
-                        posicion = []
-                        posicion_robot = robot[3]
-                        posicion.append((posicion_robot[0] + mov[0], posicion_robot[1] + mov[1]))
-                        if posicion[0] in tuneles: 
-                            lista_acciones.append((robot[0], "mover", tuple(posicion[0]))) #Devuelve el robot, la accion y la posicion adonde se mueve
+                if robot[1] == "escaneador":                            #Pregunta si es escaneador 
+                    if robot[2] >= 100:                                 #Si la batería es mayor a 100, si todavia tiene un movimiento
+                        for mov in movimientos_posibles:                #Saca los movimientos que puede hacer
+                            posicion = []
+                            posicion_robot = robot[3]
+                            posicion.append((posicion_robot[0] + mov[0], posicion_robot[1] + mov[1]))
+                            if posicion[0] in tuneles: 
+                                lista_acciones.append((robot[0], "mover", tuple(posicion[0]))) #Devuelve el robot, la accion y la posicion adonde se mueve
                 else:
-                    for robot_sin_carga in lrobots:    
-                        if robot_sin_carga[1] == "escaneador" and robot_sin_carga[2] < 100 and robot[3] == robot_sin_carga[3]: #Pregunta si hay un escaneador con poca bateria
+                    for robot_sin_carga in lrobots:                     #Busca un robot que tenga poca batería
+                        if robot_sin_carga[1] == "escaneador" and robot_sin_carga[2] < 1000 and robot[3] == robot_sin_carga[3]: #Pregunta si hay un escaneador con poca bateria
                             lista_acciones.append((robot[0], "cargar", robot_sin_carga[0]))
                     for mov in movimientos_posibles:
                         posicion = []
@@ -42,7 +43,7 @@ def planear_escaneo(tuneles, robots):
                         x = posicion_robot[0] + mov[0]
                         y = posicion_robot[1] + mov[1]
                         posicion.append((x,y))
-                        if posicion[0] in tuneles_completos: 
+                        if posicion[0] in tuneles: 
                             lista_acciones.append((robot[0], "mover", tuple(posicion[0])))
 
             return lista_acciones
@@ -56,28 +57,26 @@ def planear_escaneo(tuneles, robots):
 
             lrobots = list(list(x) for x in lrobots)
             ltuneles = list(ltuneles)
-            energia = 0
+            energia = 1000
 
             if accion == "mover":
                 for indice, robot in enumerate(lrobots):
                     if robot_hace == robot[0]:
-                        nuevo_robot = list(robot)
-                        lrobots.remove(robot)
-                        if nuevo_robot[1] == "escaneador":
+                        if robot[1] == "escaneador":
                             if donde in ltuneles:
                                 ltuneles.remove(donde)
-                                energia = nuevo_robot[2] - 100
-                                nuevo_robot[2] = energia
-                        nuevo_robot[3] = donde
-                        lrobots.insert(indice, nuevo_robot)
+                            energia = robot[2] - 100
+                        robot[2] = energia
+                        robot[3] = donde
             elif accion == "cargar":
                 for indice, robot in enumerate(lrobots):
                     if robot[0] == donde:
                         robot[2] = 1000
+
             lrobots = tuple(tuple(x) for x in lrobots)
 
             nuevo_state = (tuple(ltuneles), lrobots)
-            
+
             return nuevo_state
 
 #       ---------- COST ----------
@@ -90,19 +89,15 @@ def planear_escaneo(tuneles, robots):
 
 #       ---------- HEURISTICA ----------
 
-        # def heuristic(self, state):
-        #     costo_heuristic = 0
-        #     #La heuristica corresponde a los nodos que faltan recorrer. (tunel,"L") 
-        #     # multiplicado por el tiempo del robot que menos consume(1 minuto).
-        #     tuneles, robots = state
-        #     for i_tunel, tunel in enumerate(tuneles):
-        #         if estado_listas[i_tunel] != "E":        
-        #             costo_heuristic += 1    
-        #     return costo_heuristic
+        def heuristic(self, state):
+            #Busca y calcula las coordenadas que le faltan 
+            ltuneles, lrobots = state
+            costo_heuristic = 0
+            for i_tunel, tunel in enumerate(ltuneles):
+                costo_heuristic += 1
+            return costo_heuristic
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-    tuneles_completos = tuple(tuneles)
 
     lista_robots = []
     for robot in robots:
@@ -136,8 +131,15 @@ if __name__ == '__main__':
 
     robots= [E1, S1]
 
-    MINA_TUNEL_RECTO_LARGO = (
-    (5, 1), (5, 2), (5, 3), (5, 4), (5, 5), (5, 6), (5, 7), (5, 8), (5, 9), (5, 10), (5, 11),)
+    MINA_T_LARGA = (
+                    (2, 3),
+                    (3, 3),
+                    (4, 3),
+    (5, 1), (5, 2), (5, 3),
+                    (6, 3),
+                    (7, 3),
+                    (8, 3),
+    )
 
-    plan = planear_escaneo(MINA_TUNEL_RECTO_LARGO, robots)
+    plan = planear_escaneo(MINA_T_LARGA, robots)
     print(plan)
